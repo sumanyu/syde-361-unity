ed0a75bff3b367d16c2fc321fd308be23960b70f
Sumanyu Sharma <sumanyu@hotmail.com>
2014-06-05 16:15:26 -0400

    Add matlab processing code

--- unity.py
+++ unity.py
@@ -1,16 +1,42 @@
 import sys
 
-def get_data(file_name):
-  data = []
-  with open(file_name) as f:
-    pass
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+import numpy as np
 
+def get_data(file_name):
+  data = {}
+  with open(file_name) as f:
+    input_data = f.readlines()
+
+    # Clean up data
+    # data[x] = blah
+    # data[y] = blah
   return data
+
+def process_data(data):
+  # Number of samplepoints
+  N = len(data['x'])
+
+  # sample spacing - Play around with this
+  T = 1.0 / 800.0
+  x = np.linspace(0.0, N*T, N)
+  y = 
+
+  # Frequency domain
+  yf = fft(y)
+  xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+
+  # Plotting
+  plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
+  plt.grid()
+  plt.show()
 
 def main():
   if '--data' in sys.argv:
     file_name = sys.argv[1]
     data = get_data(file_name)
+    process_data(data)
 
 if __name__ == "__main__":
   main()
