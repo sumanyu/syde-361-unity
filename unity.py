dca1d58ec232f34589cdcdbf4391911e1e70e45b
Sumanyu Sharma <sumanyu@hotmail.com>
2014-06-05 12:39:52 -0400

    Add file reading code

---
+++ unity.py
@@ -0,0 +1,16 @@
+import sys
+
+def get_data(file_name):
+	data = []
+	with open(file_name) as f:
+		pass
+
+	return data
+
+def main():
+	if '--data' in sys.argv:
+		file_name = sys.argv[1]
+		data = get_data(file_name)
+
+if __name__ == "__main__":
+	main()
