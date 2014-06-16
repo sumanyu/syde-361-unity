33eb690035a2f1f8594c9af5f44f5eecda24fd5d
Sumanyu Sharma <sumanyu@hotmail.com>
2014-06-16 13:31:45 -0400

    Plot basic data

--- unity.py
+++ unity.py
@@ -5,13 +5,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def get_data(file_name):
-  data = {}
-  with open(file_name) as f:
-    input_data = f.readlines()
+  data = {
+    'x': [],
+    'y': []
+  }
 
-    # Clean up data
-    # data[x] = blah
-    # data[y] = blah
+  with open(file_name) as f:
+    for input_data in f:
+      x, y = [int(tmp.strip()) for tmp in input_data.split(",")]  
+      data['x'].append(x)
+      data['y'].append(y)
+
+  print data
+
   return data
 
 def process_data(data):
@@ -19,22 +25,24 @@ def process_data(data):
   N = len(data['x'])
 
   # sample spacing - Play around with this
-  T = 1.0 / 800.0
-  x = np.linspace(0.0, N*T, N)
-  y = 
+  # T = 1.0 / 800.0
+  # x = np.linspace(0.0, N*T, N)
+  # y = 
 
   # Frequency domain
-  yf = fft(y)
-  xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+  # yf = fft(y)
+  # xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
   # Plotting
-  plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
+  # plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
+
+  plt.plot(data['x'], data['y'])
   plt.grid()
   plt.show()
 
 def main():
   if '--data' in sys.argv:
-    file_name = sys.argv[1]
+    file_name = sys.argv[2]
     data = get_data(file_name)
     process_data(data)
 
