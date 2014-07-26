e837b55988e7b6fde954323aec54ef29dea35ac1
Raphael Kalker <rkalker@uwaterloo.ca>
2014-07-25 20:13:12 -0400

    updated graphing.py

--- report.py
+++ report.py
@@ -2,7 +2,7 @@ import os
 import time
 import json
 
-
+data = []
 
 def openJson():
     filename = 'output_' + time.strftime("%Y-%m-%d")
@@ -10,15 +10,10 @@ def openJson():
         print filename + "file not found"
     
     with open(filename,'r') as content_file:
+
         for line in content_file:
-            print json.loads(line)
-
-
-        # content = content_file.read()
-
-    # print content
-
-
+            line = json.loads(line)
+            data.append(line)
 
 openJson()
 
