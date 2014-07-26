d304aaad9096a2fdff5f92bc05f225cf222c70f5
AnniHe <anni.he.13@gmail.com>
2014-07-26 13:29:54 -0400

    adding more UI

--- unity.py
+++ unity.py
@@ -36,6 +36,77 @@ beta_end = 32
 gamma_start = 32
 gamma_end = 64
 
+app = QtGui.QApplication([])
+mw = QtGui.QMainWindow()
+mw.setWindowTitle('Unity - MDI')
+mw.resize(100,100)
+cw = QtGui.QWidget()
+mw.setCentralWidget(cw)
+
+#Create layout to manage widgets size and positions
+layout = QtGui.QGridLayout()
+cw.setLayout(layout)
+
+btn_start = QtGui.QPushButton('Start Meditation')
+btn_stop = QtGui.QPushButton('End Session')
+layout.addWidget(btn_start,0,0)
+layout.addWidget(btn_stop,1,0)
+
+plot = pg.PlotWidget()
+mw.show()
+btn_start.clicked.connect(btnclick_start)
+btn_stop.clicked.connect(btnclick_stop)
+
+def updateUI():
+    #do stuff for ui if needed
+    print "updateUI clicked"
+
+def btnclick_start():
+    nr = layout.count()
+    for i in reversed(range(nr, 1, -1)):
+        layout.itemAt(i).widget().deleteLater()
+
+    #TODO: resizing doesn't work as expected
+    mw.resize(100,100)
+    mw.show()
+
+    btn_stop.setDisabled(True)
+    btn_stop.setDisabled(False)
+    #add function here to start calibration
+    updateUI()
+
+def btnclick_stop():
+    btn_stop.setDisabled(True)
+    btn_start.setDisabled(False)
+    data = np.array(random_generator(1000))
+
+    #get data here
+    for i in range(3):
+        report_plot(i,data)
+
+def report_plot(nr, data):
+    y = data
+    #y_max = np.hstack(y).max(axis = 0)
+    #x_max = y.size//512
+    y_max = 1
+    x_max = 120
+
+    pw_report = pg.PlotWidget()
+    layout.addWidget(pw_report, nr + 3, 0)
+    pw_report.setLabel('left', 'Magnitude', units='dB')
+    pw_report.setLabel('bottom', 'Time', units='s')
+    # pw_report.setXRange(0, x_max)
+    # pw_report.setYRange(0, y_max)
+    pw_report.plot(y)
+
+    #fiddle with this for additional plots
+    mw.resize(500,500)
+
+def random_generator(n):
+    data_x = np.random.random(n)
+    print data_x
+    return data_x
+
 def get_data(file_name):
   data = {
     'eeg': [],
@@ -578,8 +649,9 @@ def main():
     layout.addWidget(btn_stop,1,0)
 
     plot = pg.PlotWidget()
-
     mw.show()
+    btn_start.clicked.connect(btnclick_start)
+    btn_stop.clicked.connect(btnclick_stop)
     QtGui.QApplication.instance().exec_()
 
 
@@ -589,6 +661,8 @@ def main():
     for num in range(1, 11):
         os.remove(pipe + str(num))
     print "main exits"
+
+
 
 if __name__ == "__main__":
   main()
